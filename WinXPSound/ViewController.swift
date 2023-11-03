0fd6e201863c3230eea9c13bfe1fafa512d42387
Anu <anupambhartiab@gmail.com>
2023-11-03 22:19:57 +0530

    Startup sound for xp added and working on app launch

--- WinXPSound/ViewController.swift
+++ WinXPSound/ViewController.swift
@@ -6,20 +6,46 @@
 //
 
 import Cocoa
+import AVFoundation
 
-class ViewController: NSViewController {
 
+class ViewController: NSViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        print("Working")
+        
+        guard let url = Bundle.main.url(forResource: "windows-xp-startup-sound", withExtension: "mp3") else {
+            print("Sound file not found")
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        } catch {
+            print("Error playing sound: \(error.localizedDescription)")
+        }
+        
+        
     }
-
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+            if flag {
+                print("Playback finished successfully.")
+            } else {
+                print("Playback did not finish successfully.")
+            }
+        }
 
 
 }
